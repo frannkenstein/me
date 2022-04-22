662eacaab812483d99fa0225bf14f3033c4a4214
frannkenstein <deathakanote@gmail.com>
2022-04-22 16:38:49 +0530

    check 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 function App() {
   const [state, setState] = React.useState("red");
   const colorList = ["green", "blue", "yellow", "red", "purple"];
-  return <div className="App">Aakash Singh 1 2 2 2 2 2 
-  
-  <div> Singh Aakash </div>
-   </div>;
+  return (
+    <div className="App">
+      Aakash Singh
+      <div>One two</div>
+    </div>
+  );
 }
 
 export default App;

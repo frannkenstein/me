aa2b58c27921f5fd3a9ae3204c4bc4bf242f52f7
Aakash <deathakanote@gmail.com>
2022-04-22 16:37:39 +0530

    Update App.js
    check 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from "react";
 function App() {
   const [state, setState] = React.useState("red");
   const colorList = ["green", "blue", "yellow", "red", "purple"];
-  return <div className="App">Aakash Singh</div>;
+  return <div className="App">Aakash Singh 1 2 2 2 2 2 
+  
+  <div> Singh Aakash </div>
+   </div>;
 }
 
 export default App;
